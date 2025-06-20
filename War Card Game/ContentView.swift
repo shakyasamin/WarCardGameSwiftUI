2549ddf3239c84a7c7dee93805128233d777271c
saminshakya <saminshakya27@gmail.com>
2025-06-20 14:34:35 +0545

    completed war card game

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -12,8 +12,8 @@ struct ContentView: View {
     @State var playerCard = "card7"
     @State var cpuCard = "card13"
     
-    var playerScore = 0
-    var cpuScore = 0
+    @State var playerScore = 0
+    @State var cpuScore = 0
     
     var body: some View {
         
@@ -88,14 +88,23 @@ struct ContentView: View {
     
     func deal() {
         //Randomize the players card
-        playerCard = "card" + String(Int.random(in: 2...14))
+        var playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
         
         //Randomize the cpu card
-        cpuCard = "card" + String(Int.random(in: 2...14))
-        
+        var cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
         
         //update the scores
-        
+        if playerCardValue > cpuCardValue {
+            //Add 1 to playerscore card
+            playerScore += 1
+        }
+        else if cpuCardValue > playerCardValue{
+            //Add 1 to cpu card value
+            cpuScore += 1
+        }
+
         
     }
 }
